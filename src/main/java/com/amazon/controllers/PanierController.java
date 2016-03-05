1384e106e62bf19dd46da7de9705330572207447
Droopy06 <lamootalexandre@gmail.com>
2016-03-05 06:09:46 +0100

    add panier
    add panier

--- src/main/java/com/amazon/controllers/PanierController.java
+++ src/main/java/com/amazon/controllers/PanierController.java
@@ -6,16 +6,19 @@ import com.amazon.services.ArticlesService;
 import com.amazon.services.CategorieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by LAMOOT Alexandre on 04/03/2016.
@@ -30,15 +33,32 @@ public class PanierController {
     CategorieService categorieService;
 
     @RequestMapping(value = {"/panier/ajout/{article}"}, method = RequestMethod.GET)
-    public ModelAndView addPanier(@PathVariable(value = "article") long idProduit,
+    public String addPanier(@PathVariable(value = "article") long idProduit,
                                   HttpServletResponse response) {
+        response.addCookie(new Cookie("panier",String.valueOf(idProduit)));
+        return "redirect:/panier";
+    }
+
+    @RequestMapping(value = {"/panier"}, method = RequestMethod.GET)
+    public ModelAndView myPanier(HttpServletRequest request,
+                                 HttpServletResponse response){
         HashMap<String, Object> model = new HashMap<String, Object>();
-        /*List<Articles> articlesList = new ArrayList<>();
-        articlesList.add(articlesService.getArticlesById(idProduit));*/
-        Cookie cookie = new Cookie("panier",String.valueOf(articlesService.getArticlesById(idProduit).getId()));
-        response.addCookie(cookie);
+        List<Articles> articles = new ArrayList<>();
+        Cookie[] panierCookie = request.getCookies();
+        if(!Objects.equals(panierCookie[0].getValue(), ""))
+            articles.add(articlesService.getArticlesById(Long.valueOf(panierCookie[0].getValue())));
+        model.put("articles",articles);
         model.put("support",articlesService.getAllArticles());
         model.put("allcategories",categorieService.getAllCategories());
         return new ModelAndView("amazon/checkout/panier",model);
+    }
+
+    @RequestMapping(value = {"/panier/delete"}, method = RequestMethod.GET)
+    public String deletePanier(@CookieValue(value = "panier", defaultValue = "0") String panierCookie,
+                            HttpServletResponse response) {
+        Cookie cookie = new Cookie("panier","");
+        cookie.setMaxAge(-1);
+        response.addCookie(cookie);
+        return "redirect:/panier";
     }
 }
